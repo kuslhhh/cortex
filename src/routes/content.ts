24978126e0677a27221d8a19f7e00b970123c716
kuslhhh <kushaljadhav7745@gmail.com>
2025-02-02 16:19:37 +0530

    Added v1 progress

--- src/routes/content.ts
+++ src/routes/content.ts
@@ -23,4 +23,45 @@ router.post("/content", userMiddleware, async (req: Request, res: Response) => {
     }
 });
 
-export default router
+
+router.get("/content", userMiddleware, async (req: Request, res: Response) => {
+
+    try {
+        // @ts-ignore
+        const userId = req.userId;
+        const content = await ContentModel.find({
+            userId: userId
+        }).populate("userId", "username")
+        res.json({
+            content
+        })
+
+
+    } catch (error) {
+        console.error("Error getting content:", error);
+        res.status(500).json({ message: "Error getting content", error });
+    }
+});
+
+router.delete("/content", userMiddleware, async (req: Request, res: Response) => {
+
+    try {
+
+        const contentId = req.body.contentId;
+
+        await ContentModel.deleteMany({
+            contentId,
+            // @ts-ignore
+            userId: req.userId
+        })
+        res.json({
+            message: "Deleted Content"
+        })
+
+    } catch (error) {
+        console.error("Error getting content:", error);
+        res.status(500).json({ message: "Error getting content", error });
+    }
+});
+
+export default router;
